9aa571713025549b38240fcc78bb8061b608683a
shooontan <logo.bee.doo19@gmail.com>
2020-05-11 20:06:38 +0900

    add .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const withTM = require('next-transpile-modules')(['three/examples/jsm']);
 
 module.exports = withTM({
   env: {
-    APP_URL: 'https://modelv.mahoroi.com/',
-    APP_TITLE: 'ModelV.',
-    APP_DESCRIPTION: 'webカメラとブラウザでバーチャルモデルになれるwebアプリ',
+    APP_URL: process.env.APP_URL,
+    APP_TITLE: process.env.APP_TITLE,
+    APP_DESCRIPTION: process.env.APP_DESCRIPTION,
   },
   webpack: (config) => {
     return {
